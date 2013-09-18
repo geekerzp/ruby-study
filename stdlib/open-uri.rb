8c90cfa243f9bc416bea8bf2e3ed7e4ed716f595
geekerzp <geekerzp@gmail.com>
2013-09-18 15:24:03 +0800

    Update open-uri.rb

--- stdlib/open-uri.rb
+++ stdlib/open-uri.rb
@@ -27,3 +27,43 @@
     puts res.header['set-cookie']  
     #返回的html body  
     puts res.body  
+
+# https-仅验证服务器
+# ~~~~~~~~~~~~~~~~~~
+
+    require 'net/https'
+    require 'uri'
+ 
+    uri = URI.parse('https://liuwm-pc:8081/2.html')
+ 
+    http = Net::HTTP.new(uri.host, uri.port)
+ 
+    http.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE #这个也很重要
+ 
+    http.start {
+      http.request_get(uri.path) {|res|
+        print res.body
+      }
+    }
+    
+# https-双向验证
+# ~~~~~~~~~~~~~~
+
+    require 'net/https'
+    require 'uri'
+ 
+    uri = URI.parse('https://liuwm-pc:8081/2.html')
+ 
+    http = Net::HTTP.new(uri.host, uri.port)
+ 
+    http.use_ssl = true if uri.scheme == "https"  # enable SSL/TLS
+    http.cert =OpenSSL::X509::Certificate.ne(File.read("D:/111/client.crt"))
+    http.key =OpenSSL::PKey::RSA.new((File.read("D:/111/client.key")), "123456")# key and password
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE #这个也很重要
+ 
+    http.start {
+      http.request_get(uri.path) {|res|
+        print res.body
+      }
+    }
