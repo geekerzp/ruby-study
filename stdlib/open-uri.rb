38117fd56f92f1928387661241e0c8de8cec1177
geekerzp <geekerzp@gmail.com>
2013-09-18 15:15:24 +0800

    Update open-uri.rb

--- stdlib/open-uri.rb
+++ stdlib/open-uri.rb
@@ -1,2 +1,29 @@
-#
-# Ruby发送HTTP请求
+# ====================
+# | Ruby发送HTTP请求 |
+# ====================
+
+# GET
+# ~~~
+
+    require "open-uri"  
+      
+    #如果有GET请求参数直接写在URI地址中  
+    uri = 'http://uri'  
+    html_response = nil  
+    open(uri) do |http|  
+      html_response = http.read  
+    end  
+    puts html_response  
+    
+# POST
+# ~~~~
+
+    params = {}  
+    params["name"] = 'Tom'  
+    uri = URI.parse("http://uri")  
+    res = Net::HTTP.post_form(uri, params)   
+      
+    #返回的cookie  
+    puts res.header['set-cookie']  
+    #返回的html body  
+    puts res.body  
